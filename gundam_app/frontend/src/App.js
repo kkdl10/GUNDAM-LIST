e78ecc8368030f8b5854fe9e5f6b4c487f0697a6
Kasier De Leon <deleonkasier10@gmail.com>
2025-03-24 21:52:14 +0800

    ayoko na

---
+++ gundam_app/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GundamList from './components/GundamList';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>My Gundam Collection</h1>
+            <GundamList />
+        </div>
+    );
+}
+
+export default App;
